6b054ec52328ac7dc65385ea790893938159f1e5
ben lamothe <zonotope@gmail.com>
2015-01-04 04:02:47 -0500

    bump cider nrepl

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]]
+{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]]
         :dependencies [[com.cemerick/piggieback "0.1.3"]
                        [weasel "0.3.0"]]
         :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
