ad4943f84aea907f7addfe2e72b052a07b094872
ben lamothe <zonotope@gmail.com>
2015-01-10 01:49:51 -0500

    bump weasel version.

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,4 +1,4 @@
 {:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]]
         :dependencies [[com.cemerick/piggieback "0.1.3"]
-                       [weasel "0.3.0"]]
+                       [weasel "0.4.2"]]
         :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
