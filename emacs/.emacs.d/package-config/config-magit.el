94b5cc6a6e372f65d91104d84d96ee6bce796124
ben lamothe <zonotope@gmail.com>
2015-04-14 01:41:01 -0400

    diminish MRev

--- emacs/.emacs.d/package-config/config-magit.el
+++ emacs/.emacs.d/package-config/config-magit.el
@@ -1,5 +1,6 @@
-(require 'diminish)
+(require 'magit)
 
 ;; i don't need to be alerted that magit auto revert is running in every buffer
 ;; from a git project
-;; (diminish 'magit-auto-revert-mode)
+(require 'diminish)
+(diminish 'magit-auto-revert-mode)
