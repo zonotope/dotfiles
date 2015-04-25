9c1cd5bb07b6b72765ef103236aeea77ecf1ee46
ben lamothe <zonotope@gmail.com>
2015-04-24 20:00:12 -0400

    remove clojurescript specific deps

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,4 +1,1 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]]
-        :dependencies [[com.cemerick/piggieback "0.1.3"]
-                       [weasel "0.4.2"]]
-        :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
+{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]]}}
