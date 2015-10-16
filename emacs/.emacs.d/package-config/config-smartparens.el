855dcfb73d2c3c820aae33caeb18ea7bc4e9e22b
ben lamothe <zonotope@gmail.com>
2015-10-16 12:58:00 -0400

    sp config for elixir

--- emacs/.emacs.d/package-config/config-smartparens.el
+++ emacs/.emacs.d/package-config/config-smartparens.el
@@ -86,3 +86,13 @@
 ;;; lisp modes
 (sp-with-modes sp--lisp-modes
   (sp-local-pair "(" nil :bind "C-("))
+
+;;; elixir mode
+(sp-with-modes '(elixir-mode)
+  (sp-local-pair "fn" "end"
+         :when '(("SPC" "RET"))
+         :actions '(insert navigate))
+  (sp-local-pair "do" "end"
+         :when '(("SPC" "RET"))
+         :post-handlers '(sp-ruby-def-post-handler)
+         :actions '(insert navigate)))
