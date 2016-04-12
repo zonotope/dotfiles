cd76b325c9ae0ee0fdd18b6ef6a7c887bf04548a
ben lamothe <zonotope@gmail.com>
2016-04-12 00:58:25 -0400

    switch magenta and yellow

--- emacs/.emacs.d/package-config/config-avy.el
+++ emacs/.emacs.d/package-config/config-avy.el
@@ -4,5 +4,5 @@
 
 (set-face-attribute 'avy-lead-face nil :foreground "orange" :background nil)
 (set-face-attribute 'avy-lead-face-0 nil :foreground "green" :background nil)
-(set-face-attribute 'avy-lead-face-1 nil :foreground "yellow" :background nil)
-(set-face-attribute 'avy-lead-face-2 nil :foreground "magenta" :background nil)
+(set-face-attribute 'avy-lead-face-1 nil :foreground "magenta" :background nil)
+(set-face-attribute 'avy-lead-face-2 nil :foreground "yellow" :background nil)
