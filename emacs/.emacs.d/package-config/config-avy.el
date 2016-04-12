31baf8edc8d95bb72f8f086d162c9fc21fb3aa38
ben lamothe <zonotope@gmail.com>
2016-04-12 02:16:46 -0400

    more avy config

--- emacs/.emacs.d/package-config/config-avy.el
+++ emacs/.emacs.d/package-config/config-avy.el
@@ -1,8 +1,15 @@
 (require 'avy)
 
+;; use all letters for jump sequences
+(setq avy-keys (number-sequence ?a ?z))
+
+;; show only one character from the jump sequence at a time
+(setq avy-style 'at)
+
+;; dim the background
 (setq avy-background t)
 
-(set-face-attribute 'avy-lead-face nil :foreground "orange" :background nil)
-(set-face-attribute 'avy-lead-face-0 nil :foreground "green" :background nil)
-(set-face-attribute 'avy-lead-face-1 nil :foreground "magenta" :background nil)
+(set-face-attribute 'avy-lead-face nil :foreground "green" :background nil)
+(set-face-attribute 'avy-lead-face-0 nil :foreground "orange" :background nil)
+(set-face-attribute 'avy-lead-face-1 nil :foreground "red" :background nil)
 (set-face-attribute 'avy-lead-face-2 nil :foreground "yellow" :background nil)
