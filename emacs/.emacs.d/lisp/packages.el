7388ddff6b9994c85192d8a6860cf64bbc8931c9
ben lamothe <zonotope@gmail.com>
2016-08-16 09:23:06 -0400

    use correct melpa repo address

--- emacs/.emacs.d/lisp/packages.el
+++ emacs/.emacs.d/lisp/packages.el
@@ -4,7 +4,7 @@
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
                          ("tromey" . "http://tromey.com/elpa/")))
 (package-initialize)
 
