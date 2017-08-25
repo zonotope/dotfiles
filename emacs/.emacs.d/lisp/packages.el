fba2f5e4ef19ebe4f173a425933a45bf76e2c900
Benjamin Lamothe <blamothe@mildred.local>
2017-08-24 22:11:41 -0400

    add back ivy/council config

--- emacs/.emacs.d/lisp/packages.el
+++ emacs/.emacs.d/lisp/packages.el
@@ -211,7 +211,17 @@
 
 ;; counsel: ivy-fied versions of basic emacs commands
 (use-package counsel
-  :bind (("C-c k" . counsel-ag)))
+  :bind (("M-x" . counsel-M-x)
+         ("C-x C-f" . counsel-find-file)
+         ("<f1> f" . counsel-describe-function)
+         ("<f1> v" . counsel-describe-variable)
+         ("<f1> l" . counsel-load-library)
+         ("<f2> i" . counsel-info-lookup-symbol)
+         ("<f2> u" . counsel-unicode-char)
+         ("C-c g" . counsel-git)
+         ("C-c j" . counsel-git-grep)
+         ("C-c k" . counsel-ag)
+         ("C-x l" . counsel-locate)))
 
 (use-package ivy
   :bind ("C-c C-r" . ivy-resume)
