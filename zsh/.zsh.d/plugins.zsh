d95c34539e7a56cc84187eb18de20697368d1a97
ben lamothe <bl@732.one>
2019-03-19 02:48:14 -0400

    switch to spaceship prompt

--- zsh/.zsh.d/plugins.zsh
+++ zsh/.zsh.d/plugins.zsh
@@ -50,7 +50,11 @@ antigen bundle robbyrussell/oh-my-zsh plugins/web-search
 antigen bundle zsh-users/zsh-syntax-highlighting
 
 ## set the prompt
-antigen bundle zonotope/zsh-prompt
+### FIXME: Setting this option here because of a possible definition order bug
+###        that doesn't show the first prefix otherwise
+SPACESHIP_PROMPT_FIRST_PREFIX_SHOW="true"
+antigen theme denysdovhan/spaceship-prompt
+# antigen bundle zonotope/zsh-prompt
 
 ## history search
 antigen bundle zsh-users/zsh-history-substring-search
@@ -65,10 +69,82 @@ antigen apply
 # configure plugins                                                        #
 ############################################################################
 
+#### Spaceship Prompt
+source $ZSHD/prompt_fns.zsh
+
+SPACESHIP_PROMPT_PREFIXES_SHOW="true"
+
+# SPACESHIP_EXEC_TIME_SUFFIX=$NEWLINE
+
+SPACESHIP_USER_SHOW=always
+SPACESHIP_USER_COLOR=cyan
+SPACESHIP_USER_COLOR_ROOT=red
+SPACESHIP_USER_PREFIX=''
+SPACESHIP_USER_SUFFIX=''
+
+SPACESHIP_HOST_SHOW=always
+SPACESHIP_HOST_COLOR=magenta
+SPACESHIP_HOST_PREFIX=@
+SPACESHIP_HOST_SUFFIX=''
+
+SPACESHIP_DIR_TRUNC=0
+SPACESHIP_DIR_TRUNC_REPO=true
+SPACESHIP_DIR_COLOR=blue
+SPACESHIP_DIR_PREFIX=:
+
+SPACESHIP_GIT_STATUS_COLOR=magenta
+SPACESHIP_GIT_STATUS_ADDED=^
+SPACESHIP_GIT_STATUS_MODIFIED=*
+SPACESHIP_GIT_STATUS_AHEAD=↑
+SPACESHIP_GIT_STATUS_BEHIND=↓
+
+
+SPACESHIP_HG_SHOW=false
+SPACESHIP_PACKAGE_SHOW=false
+SPACESHIP_XCODE_SHOW_LOCAL=false
+SPACESHIP_XCODE_SHOW_GLOBAL=false
+SPACESHIP_SWIFT_SHOW_LOCAL=false
+SPACESHIP_SWIFT_SHOW_GLOBAL=false
+SPACESHIP_DOTNET_SHOW=false
+SPACESHIP_BATTERY_SHOW=false
+
+SPACESHIP_PROMPT_ORDER=(
+    exit_code     # Exit code section
+    exec_time     # Execution time
+    prompt_sep    # Dashed line to separate prompts
+    time          # Time stamps section
+    user          # Username section
+    host          # Hostname section
+    dir           # Current directory section
+    git           # Git section (git_branch + git_status)
+    package       # Package version
+    node          # Node.js section
+    ruby          # Ruby section
+    elixir        # Elixir section
+    swift         # Swift section
+    golang        # Go section
+    rust          # Rust section
+    haskell       # Haskell Stack section
+    julia         # Julia section
+    docker        # Docker section
+    aws           # Amazon Web Services section
+    venv          # virtualenv section
+    conda         # conda virtualenv section
+    pyenv         # Pyenv section
+    kubecontext   # Kubectl context section
+    terraform     # Terraform workspace section
+    line_sep      # Line break
+    jobs          # Background jobs indicator
+    char          # Prompt character
+)
+
+#### zsh autosuggestions
 ZSH_AUTOSUGGEST_USE_ASYNC=true # fetch autosuggestions asynchronously
 
+#### zsh syntax highliters
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets) # set syntax highlighters
 
+#### zsh history substring search
 HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND=(fg:yellow) # color matches yellow
 bindkey '^[[A' history-substring-search-up # up arrow key for search up
 bindkey '^[[B' history-substring-search-down # down arrow key for search down
---
+++ zsh/.zsh.d/prompt_fns.zsh
@@ -0,0 +1,23 @@
+#
+# Prompt separator
+#
+
+# ------------------------------------------------------------------------------
+# Configuration
+# ------------------------------------------------------------------------------
+
+SPACESHIP_PROMPT_SEP_SHOW="${SPACESHIP_PROMPT_SEP_SHOW=true}"
+SPACESHIP_PROMPT_SEP_PREFIX="${SPACESHIP_PROMPT_SEP_PREFIX="$NEWLINE"}"
+SPACESHIP_PROMPT_SEP_SUFFIX="${SPACESHIP_PROMPT_SEP_SUFFIX="$NEWLINE"}"
+SPACESHIP_PROMPT_SEP_COLOR="${SPACESHIP_PROMPT_SEP_COLOR="white"}"
+
+# ------------------------------------------------------------------------------
+# Section
+# ------------------------------------------------------------------------------
+
+spaceship_prompt_sep() {
+    spaceship::section ${SPACESHIP_PROMPT_SEP_COLOR} \
+                       ${SPACESHIP_PROMPT_SEP_PREFIX} \
+                       "-------------------------------------------------------" \
+                       ${SPACESHIP_PROMPT_SEP_SUFFIX}
+}
