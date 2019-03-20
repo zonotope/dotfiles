d7cb173e89d0d825aab8c2ef42974f61453246c5
ben lamothe <bl@732.one>
2019-03-20 03:48:08 -0400

    zsh prompt tweaks

--- zsh/.zsh.d/plugins.zsh
+++ zsh/.zsh.d/plugins.zsh
@@ -53,6 +53,7 @@ antigen bundle zsh-users/zsh-syntax-highlighting
 ### FIXME: Setting this option here because of a possible definition order bug
 ###        that doesn't show the first prefix otherwise
 SPACESHIP_PROMPT_FIRST_PREFIX_SHOW="true"
+SPACESHIP_PROMPT_DEFAULT_PREFIX=""
 antigen theme denysdovhan/spaceship-prompt
 # antigen bundle zonotope/zsh-prompt
 
@@ -72,32 +73,49 @@ antigen apply
 #### Spaceship Prompt
 source $ZSHD/prompt_fns.zsh
 
-SPACESHIP_PROMPT_PREFIXES_SHOW="true"
-
 # SPACESHIP_EXEC_TIME_SUFFIX=$NEWLINE
 
+SPACESHIP_CHAR_SYMBOL="%# ➜ "
+SPACESHIP_CHAR_SYMBOL_SECONDARY="..➜ "
+
 SPACESHIP_USER_SHOW=always
-SPACESHIP_USER_COLOR=cyan
-SPACESHIP_USER_COLOR_ROOT=red
-SPACESHIP_USER_PREFIX=''
-SPACESHIP_USER_SUFFIX=''
+SPACESHIP_USER_COLOR=white
+SPACESHIP_USER_COLOR_ROOT=white
+SPACESHIP_USER_PREFIX=
+SPACESHIP_USER_SUFFIX=
 
 SPACESHIP_HOST_SHOW=always
-SPACESHIP_HOST_COLOR=magenta
-SPACESHIP_HOST_PREFIX=@
-SPACESHIP_HOST_SUFFIX=''
+SPACESHIP_HOST_COLOR=white
+SPACESHIP_HOST_PREFIX=%F{white}@%F{}
+SPACESHIP_HOST_SUFFIX=
 
 SPACESHIP_DIR_TRUNC=0
 SPACESHIP_DIR_TRUNC_REPO=true
 SPACESHIP_DIR_COLOR=blue
-SPACESHIP_DIR_PREFIX=:
+SPACESHIP_DIR_PREFIX="%F{white}[%F{}"
+SPACESHIP_DIR_SUFFIX="%F{white}]%F{}"
 
+SPACESHIP_GIT_BRANCH_COLOR=magenta
 SPACESHIP_GIT_STATUS_COLOR=magenta
 SPACESHIP_GIT_STATUS_ADDED=^
 SPACESHIP_GIT_STATUS_MODIFIED=*
 SPACESHIP_GIT_STATUS_AHEAD=↑
 SPACESHIP_GIT_STATUS_BEHIND=↓
 
+SPACESHIP_GIT_PREFIX="%F{white} [%F{}"
+SPACESHIP_GIT_SUFFIX="%F{white}]%F{}"
+
+SPACESHIP_GIT_STATUS_PREFIX=" "
+SPACESHIP_GIT_STATUS_SUFFIX=""
+
+SPACESHIP_DOCKER_PREFIX="%F{white} [%F{}"
+SPACESHIP_DOCKER_SUFFIX="%F{white}]%F{}"
+
+SPACESHIP_GOLANG_PREFIX="%F{white} [%F{}"
+SPACESHIP_GOLANG_SUFFIX="%F{white}]%F{}"
+
+SPACESHIP_AWS_PREFIX="%F{white} [%F{}"
+SPACESHIP_AWS_SUFFIX="%F{white}]%F{}"
 
 SPACESHIP_HG_SHOW=false
 SPACESHIP_PACKAGE_SHOW=false
@@ -113,8 +131,6 @@ SPACESHIP_PROMPT_ORDER=(
     exec_time     # Execution time
     prompt_sep    # Dashed line to separate prompts
     time          # Time stamps section
-    user          # Username section
-    host          # Hostname section
     dir           # Current directory section
     git           # Git section (git_branch + git_status)
     package       # Package version
@@ -134,8 +150,14 @@ SPACESHIP_PROMPT_ORDER=(
     kubecontext   # Kubectl context section
     terraform     # Terraform workspace section
     line_sep      # Line break
+    line_sep      # Line break
     jobs          # Background jobs indicator
     char          # Prompt character
+)
+
+SPACESHIP_RPROMPT_ORDER=(
+    user          # Username section
+    host          # Hostname section
 )
 
 #### zsh autosuggestions
